c85b3acba5d01e91dc2caa5d817c8b8f57a860ed
unknown <q_cozic@hotmail.fr>
2022-12-07 15:28:24 +0100

    r

--- src/global/Social.js
+++ src/global/Social.js
@@ -4,37 +4,20 @@ import EmailIcon from '@mui/icons-material/Email';
 import './Global.css'
 import { Box } from '@mui/material'
 import { theme } from '../theme'
-import {  useState } from 'react';
+import { useState } from 'react'
 
 
 export const Social = () => {
-
-    const palette = theme()
-
     const items = [<GitHubIcon sx={{ fontSize: 14 }} />, <LinkedInIcon sx={{ fontSize: 14 }} />, <EmailIcon sx={{ fontSize: 14 }} />, <Box sx={{ fontSize: 10, fontWeight: 700 }}>CV</Box>]
     
-    const arr = new Array(items.length)
-    arr.fill(palette.primary.dark, 0)
+    const palette = theme()
 
-    const [color, setColor] = useState(arr)
-    
-    const onMouseEnter = (index) =>{
-        const newcolor = arr
-        newcolor[index] = palette.primary.light
-        setColor(newcolor)
-        console.log('r')
-    }
-    const onMouseLeave = (index) =>{
-        const newcolor = arr
-        newcolor[index] = palette.primary.dark
-        setColor(newcolor)
-    }
-    
-    
     const listeSocial = items.map((item, index) => 
-    <li  key={index} onMouseEnter={()=> onMouseEnter(index)} onMouseLeave={()=> onMouseLeave(index)} style={{ color: color[index] }}>
+    <li  key={index} >
+        <div style={{ cursor: 'pointer'}}>
             {item}
-        </li>
+        </div>
+    </li>
     )
     return (
         <div className='social'>
